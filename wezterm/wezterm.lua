66c012516c5feb94d9cd0e2e0894a55200dd6766
Guedxx <pedrogg@id.uff.br>
2026-05-16 16:01:41 -0300

    Update WezTerm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,7 +8,7 @@ local config = wezterm.config_builder()
 
 -- Platform / rendering
 config.enable_wayland = true
-config.front_end = 'WebGpu'
+config.front_end = 'OpenGL'
 config.webgpu_power_preference = 'HighPerformance'
 config.max_fps = 120
 config.animation_fps = 60
@@ -24,7 +24,7 @@ config.harfbuzz_features = { 'calt=1', 'clig=1', 'liga=1' }
 
 -- Window / appearance
 config.color_scheme = 'Ayu Dark (Gogh)'
-config.window_background_opacity = 0.96
+config.window_background_opacity = 0.80
 config.text_background_opacity = 1.0
 config.window_padding = {
   left = 10,
@@ -37,11 +37,9 @@ config.initial_rows = 34
 config.adjust_window_size_when_changing_font_size = false
 config.hide_mouse_cursor_when_typing = true
 config.window_close_confirmation = 'NeverPrompt'
-config.window_decorations = 'TITLE|RESIZE'
 
 -- Tabs
 config.enable_tab_bar = true
-config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = false
 config.tab_max_width = 32
@@ -54,8 +52,9 @@ config.scrollback_lines = 10000
 
 -- Custom keys.
 config.keys = {
-  -- SUPER+\ splits the current pane side-by-side.
+  -- SUPER+\ and SUPER+/ split the current pane side-by-side.
   { key = '\\', mods = 'SUPER', action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+  { key = '/', mods = 'SUPER', action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
 
   -- SUPER+N opens a new tab.
   { key = 'n', mods = 'SUPER', action = act.SpawnTab 'CurrentPaneDomain' },
